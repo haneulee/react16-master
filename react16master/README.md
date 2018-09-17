26bf539d29881ba2c5b01ec0ed14ee62ce0f83b6
Haneul Lee <lovesky4294@gmail.com>
2018-09-18 01:07:27 +0900

    hoc error boundaries

--- react16master/README.md
+++ react16master/README.md
@@ -5,3 +5,5 @@ React 16
 - Fragment 태그
 - return types string
 - 리액트 루트 밖을 수정 가능한 portals (ex. iframe, html 변경, 워드프레스, 리액트 플러그인)
+- error boundaries componentDidCatch (ex. 에러 대응 가능)
+- HOC 으로 컴포넌트 보호 가능 (ex. 에러가 난 컴포넌트만 error fallback 이 보이고, 전체 앱은 정상동작)
--- react16master/src/App.js
+++ react16master/src/App.js
@@ -1,11 +1,50 @@
 import React, { Component, Fragment } from "react";
 import { createPortal } from "react-dom";
 
+const BoundaryHOC = ProtectedComponent =>
+  class Boundary extends Component {
+    state = {
+      hasError: false
+    };
+    componentDidCatch(error, info) {
+      console.log(`catched ${error} that i have is ${JSON.stringify(info)}`);
+      this.setState({ hasError: true });
+    }
+
+    render() {
+      const { hasError } = this.state;
+      if (hasError) {
+        return <ErrorFallback />;
+      } else {
+        return <ProtectedComponent />;
+      }
+    }
+  };
+
+class ErrorMakers extends Component {
+  state = {
+    friends: ["a", "b", "c"]
+  };
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({ friends: undefined });
+    }, 2000);
+  }
+  render() {
+    const { friends } = this.state;
+    return friends.map(friend => `${friend}`);
+  }
+}
+
+const PErrorMakers = BoundaryHOC(ErrorMakers);
+
 class Portals extends Component {
   render() {
     return createPortal(<Message />, document.getElementById("touchme"));
   }
 }
+
+const PPortals = BoundaryHOC(Portals);
 
 const Message = () => "Just touched it!";
 
@@ -15,6 +54,8 @@ class ReturnTypes extends Component {
   }
 }
 
+const ErrorFallback = () => " Sorry, something went wrong!";
+
 class App extends Component {
   render() {
     return (
@@ -22,10 +63,12 @@ class App extends Component {
       <Fragment>
         <Portals />
         <ReturnTypes />
+        <PErrorMakers />
+        <PPortals />
       </Fragment>
       // </>
     );
   }
 }
 
-export default App;
+export default BoundaryHOC(App);
